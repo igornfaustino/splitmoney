a8403d64492bc3bf38b59654a4053f39ae40c8f9
igornfaustino <igor@nfaustino.com>
2023-12-23 21:28:48 -0300

    initial commit

---
+++ src/main/java/com/nfaustino/splitmoney/SplitmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.nfaustino.splitmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitmoneyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nfaustino/splitmoney/owes/infra/controller/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.nfaustino.splitmoney.owes.infra.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/temp")
+public class HelloWorld {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String helloWorld() {
+        return "hello world";
+    }
+
+}
